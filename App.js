755d7c8aec93a7103e8f71febb530b528f9c12fa
mukuljainmj <mukuljain0295@gmail.com>
2023-10-22 09:16:34 +0530

    ignite

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const heading1 = React.createElement('h1', { id: 'heading1' }, 'Hellow World From React Heading 1');
 const heading2 = React.createElement('h2', { id: 'heading2' }, 'Hellow World From React Heading 2');
