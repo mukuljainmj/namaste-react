e6902261a0f108d5b0d9a3ebb853b4e4f609c873
mukuljainmj <mukuljain0295@gmail.com>
2023-10-24 10:58:27 +0530

    laying foundation

--- App.js
+++ App.js
@@ -2,10 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const heading1 = React.createElement('h1', { id: 'heading1' }, 'Hellow World From React Heading 1');
-const heading2 = React.createElement('h2', { id: 'heading2' }, 'Hellow World From React Heading 2');
-const childDiv1 = React.createElement('div', {id: 'child1'}, [heading1, heading2]);
-const childDiv2 = React.createElement('div', {id: 'child2'}, [heading1, heading2]);
-const parentDiv = React.createElement('div', {id: 'parent'}, [childDiv1, childDiv2]);
-console.log(parentDiv, 'parent div');
-root.render(parentDiv);
+// const heading1 = React.createElement('h1', { id: 'heading1' }, 'Hellow World From React Heading 1');
+// const heading2 = React.createElement('h2', { id: 'heading2' }, 'Hellow World From React Heading 2');
+// const childDiv1 = React.createElement('div', {id: 'child1'}, [heading1, heading2]);
+// const childDiv2 = React.createElement('div', {id: 'child2'}, [heading1, heading2]);
+// const parentDiv = React.createElement('div', {id: 'parent'}, [childDiv1, childDiv2]);
+const jsxHeading1 = (
+  <div>
+    <h1>Hellow World</h1>
+  </div>
+);
+
+// const jsxHeading2 = React.createElement('h1', {}, console.log('hi'));
+
+const HeadingComponent1 = () => (
+  <div>
+    {console.log('one')}
+    <h1>Heading Component1</h1>
+  </div>
+);
+
+const HeadingComponent2 = () => {
+  return (
+    <div>
+      <h1>Heading Component 2</h1>
+    </div>
+  );
+};
+
+root.render(HeadingComponent1());
---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Namaste React
+
+# Parcel
+- Dev build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
